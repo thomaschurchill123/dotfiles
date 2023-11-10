7be5a8f99ef7c17de79afd94387ab2a552b37e9c
Thomas Churchill <thomas.churchill123@outlook.com>
2023-11-10 00:09:05 +0000

    added clipboard settings

--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -5,3 +5,16 @@
 local opt = vim.opt
 
 opt.relativenumber = false
+
+g.clipboard = {
+  name = "OSC 52",
+  copy = {
+    ["+"] = require("vim.clipboard.osc52").copy,
+    ["*"] = require("vim.clipboard.osc52").copy,
+  },
+  paste = {
+    ["+"] = require("vim.clipboard.osc52").paste,
+    ["*"] = require("vim.clipboard.osc52").paste,
+  },
+}
+
