c761e9e8a0292495e2857c71334bf45a42e70c1a
Thomas Churchill <thomas.churchill@cloudcraftersolutions.co.uk>
2024-10-03 21:31:49 +0100

    moved to nvchad

---
+++ .config/nvim/lua/chadrc.lua
@@ -0,0 +1,23 @@
+-- This file needs to have same structure as nvconfig.lua 
+-- https://github.com/NvChad/ui/blob/v2.5/lua/nvconfig.lua
+-- Please read that file to know all available options :( 
+
+---@type ChadrcConfig
+local M = {}
+
+M.base46 = {
+	theme = "chadracula-evondev",
+
+	-- hl_override = {
+	-- 	Comment = { italic = true },
+	-- 	["@comment"] = { italic = true },
+	-- },
+}
+
+M.options = {
+  user = function ()
+    vim.opt.clipboard = "unnamedplus"
+  end
+}
+
+return M
